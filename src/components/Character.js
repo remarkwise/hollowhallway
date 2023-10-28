023d15956cbaea91ca18aa7efef61fe0dbf4d5d5
Developing Dean <remarkwise@gmail.com>
2023-10-28 00:25:12 +0000

    Update 2 files

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,34 @@
 import { useState } from "react"
 import "../css/Character.css";
-require('dotenv').config() 
-const API_KEY = process.env.OPENAIAPIKEY; // secure -> environment variable
-console.log(API_KEY);
+
+// OpenAI key
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const secret_name = "hollowhallway/Character";
+
+const client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const API_KEY = response.SecretString; // secure -> environment variable
 
 function Character() {
   const [tweet, setTweet] = useState("");
