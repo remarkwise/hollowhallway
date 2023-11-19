cd1a0386a7e36697fc5457c0bf65c9eeaedbed3b
Developing Dean <remarkwise@gmail.com>
2023-11-19 23:35:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Plan from "./components/Plan";
 import StoryCards from "./components/StoryCards";
 
 const appTitle = "Hollow Hallway";
-const versionNumber = "0.6.11";
+const versionNumber = "0.6.12";
 
 const App = () => {
   // Form
